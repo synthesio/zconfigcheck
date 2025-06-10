0af99cf54cfead7b6bebc3f3f3df1204e9a973cf
ccoVeille <3875889+ccoVeille@users.noreply.github.com>
2025-06-10 16:49:13 +0200

    chore: fix issues with testdata package
    This missing file was causing issues with the testdata package.
    
    dependabot was not able to update the zconfig package because of this missing file.
    
    Also, the testdata/validate/foo/go.mod file was useless and was removed.

---
+++ testdata/doc.go
@@ -0,0 +1,5 @@
+// Package testdata provides a set of test data for use in unit tests.
+package testdata
+
+// We need this almost empty file to ensure that the testdata package is recognized
+// by the Go toolchain. This is necessary because Go does not allow packages without any files.
